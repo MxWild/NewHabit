09e2115efe7d604564cc696854b01ff09069809c
mxwild <mxwild@gmail.com>
2020-11-03 11:09:13 +0300

    Add Toast

--- app/src/main/java/com/gmail/mxwild/newhabit/MainActivity.kt
+++ app/src/main/java/com/gmail/mxwild/newhabit/MainActivity.kt
@@ -2,10 +2,21 @@ package com.gmail.mxwild.newhabit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainTextView = findViewById<TextView>(R.id.main_text_view)
+
+        mainTextView.setOnClickListener { showHelpWindow() }
+    }
+
+    private fun showHelpWindow() {
+        Toast.makeText(applicationContext, getString(R.string.message_future), Toast.LENGTH_LONG )
+            .show()
     }
 }
