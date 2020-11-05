1cada44902484d9210af8c55d8f25f43000c185b
mxwild <mxwild@gmail.com>
2020-11-05 11:45:08 +0300

    Add transition and fix some bugs

--- app/src/main/java/com/gmail/mxwild/newhabit/StatisticsActivity.kt
+++ app/src/main/java/com/gmail/mxwild/newhabit/StatisticsActivity.kt
@@ -1,22 +1,29 @@
 package com.gmail.mxwild.newhabit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.gmail.mxwild.newhabit.model.Statistics
 
 class StatisticsActivity : AppCompatActivity() {
+
+    companion object {
+        const val statistics = "Statistics"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
 
-        val statisticsFromParcelable: Statistics? = intent.getParcelableExtra("Statistics")
+        val statisticsFromParcelable: Statistics = intent.getParcelableExtra(statistics) ?: Statistics("Empty", 0)
 
-        val name = statisticsFromParcelable?.name
-        val count = statisticsFromParcelable?.count
+        val name = statisticsFromParcelable.name
+        val count = statisticsFromParcelable.count
 
         val findViewById = findViewById<TextView>(R.id.statistics_text_view)
 
-        findViewById.text = "${name.toString()}: $count"
+        findViewById.text = "$name: $count"
     }
+
 }
